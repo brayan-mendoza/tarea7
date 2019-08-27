1f66c8f4cd075a3500dab79e192e50b0a1d2e4a6
BRIAN MENDOZA <31576390+brayan-mendoza@users.noreply.github.com>
2019-08-27 18:15:32 -0500

    Add files via upload

---
+++ Tarea7.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+suma =7+10
+print(suma)
+resta =5-5
+print(resta)
+multiplicacion=(7*7)
+print(multiplicacion)
+division= 27/3
+print(division)
+
+
+# In[ ]:
+
+
+
+
